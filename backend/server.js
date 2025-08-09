c8d2244bde5df16e73981af347ef09a385d04f53
tahir <tahir@gmail.com>
2025-08-10 03:52:53 +0530

    mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -41,10 +41,7 @@ app.use(cookieParser());
 async function main() {
     //await mongoose.connect('mongodb://127.0.0.1:27017/ichat');
     console.log('Connected to the database');
-       mongoose.connect(db_url , {
-           useNewUrlParser: true,
-           useUnifiedTopology: true
-       })
+       mongoose.connect(db_url )
 };
 main();
 
